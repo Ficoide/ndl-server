9f527e890feff370c52f83bae97b78dcf87fa5ec
Zakaria EL OUAD <z.elouad@actongroup.com>
2023-12-07 20:18:28 +0100

    init commit

---
+++ src/test/java/com/example/ndlserver/NdlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ndlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
